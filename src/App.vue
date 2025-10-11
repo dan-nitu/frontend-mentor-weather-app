64291b521e97119a27585bdcb028b2fa52f1d0d6
Voodoo30fps <voodoo30fps@gmail.com>
2025-10-11 22:16:30 +0300

    search for city + input and button design

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,43 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const location = ref('')
+const city = ref('')
+const country = ref('')
+const temperature = ref('')
+
+const getWeather = async () => {
+  try {
+    const response = await axios.get(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${location.value}`,
+    )
+    console.log(response.data.results[0])
+    const data = response.data.results[0]
+    city.value = data.name
+    country.value = data.country
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
 
 <template>
   <header>
     <img src="@/assets/images/logo.svg" alt="Logo" />
   </header>
+
   <h1>How's the sky looking today?</h1>
+
+  <form @submit.prevent="getWeather">
+    <div class="input-with-icon">
+      <label for="search" class="icon search"></label>
+      <input id="search" type="text" v-model="location" placeholder="Search for a place..." />
+    </div>
+    <button type="submit">Search</button>
+  </form>
+
+  <div>City: {{ city }}</div>
+  <div>Country: {{ country }}</div>
+  <div>Temperature: {{ temperature }}</div>
 </template>
