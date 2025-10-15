33ef057acad40e3bfbb5e151083b22131e39fe6e
Voodoo30fps <voodoo30fps@gmail.com>
2025-10-15 13:52:42 +0300

    moved weatherCodeMap into utils file

--- src/components/GetWeatherForm.vue
+++ src/components/GetWeatherForm.vue
@@ -2,47 +2,17 @@
 import { ref, defineEmits } from 'vue'
 import axios from 'axios'
 
+import { weatherCodeMap } from '@/utils/weatherCodeMap.js'
+
 import InputWithIcon from './InputWithIcon.vue'
 import Button from './Button.vue'
 
 const emit = defineEmits()
-const weatherData = ref({})
 
 const location = ref('')
 
 const latitude = ref('')
 const longitude = ref('')
-
-const weatherCodeMap = {
-  0: 'sunny', // Clear sky
-  1: 'partly-cloudy', // Mainly clear
-  2: 'partly-cloudy', // Partly cloudy
-  3: 'overcast', // Overcast
-  45: 'fog', // Fog
-  48: 'fog', // Depositing rime fog
-  51: 'drizzle', // Light drizzle
-  53: 'drizzle', // Moderate drizzle
-  55: 'drizzle', // Heavy drizzle
-  56: 'drizzle', // Light freezing drizzle
-  57: 'drizzle', // Heavy freezing drizzle
-  61: 'rain', // Light rain
-  63: 'rain', // Moderate rain
-  65: 'rain', // Heavy rain
-  66: 'rain', // Light freezing rain
-  67: 'rain', // Heavy freezing rain
-  71: 'snow', // Light snow
-  73: 'snow', // Moderate snow
-  75: 'snow', // Heavy snow
-  77: 'snow', // Snow grains
-  80: 'rain', // Light rain showers
-  81: 'rain', // Moderate rain showers
-  82: 'rain', // Heavy rain showers
-  85: 'snow', // Light snow showers
-  86: 'snow', // Heavy snow showers
-  95: 'storm', // Thunderstorm
-  96: 'storm', // Thunderstorm with light hail
-  99: 'storm', // Thunderstorm with heavy hail
-}
 
 const getCoordinates = async () => {
   const response = await axios.get(
