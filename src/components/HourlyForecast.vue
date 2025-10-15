99d877f28ab573e24401ee3f5a60fdbec0c0c0d9
Voodoo30fps <voodoo30fps@gmail.com>
2025-10-15 13:19:10 +0300

    added hourly weather select + functionality + design

--- src/components/HourlyForecast.vue
+++ src/components/HourlyForecast.vue
@@ -1,18 +1,28 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, ref } from 'vue'
 
 defineProps({
   hourlyWeather: Array,
 })
+
+const selectedDate = ref(0)
 </script>
 
 <template>
   <section class="card hourly-forecast">
     <div class="scroll-wrapper">
-      <div>Hourly forecast</div>
+      <div class="controls-wrapper">
+        <div>Hourly forecast</div>
+
+        <select v-model="selectedDate">
+          <option v-for="(day, index) in hourlyWeather" :key="index" :value="index">
+            {{ day.day }}
+          </option>
+        </select>
+      </div>
 
       <div class="cards-wrapper">
-        <div class="card" v-for="data in hourlyWeather[0].hours">
+        <div class="card" v-for="data in hourlyWeather[selectedDate].hours">
           <div>
             <div class="image-wrapper">
               <img
